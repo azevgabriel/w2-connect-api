9ebbfac376c64752bd5d9f966740746ff62514cd
Gabriel Azevedo <azevgabriel@gmail.com>
2025-05-13 11:21:53 -0300

    feat: add pino

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,8 +3,19 @@ import express from 'express';
 import http from 'http';
 import { routes } from '../routes';
 
+import pino from 'pino';
+import pinoHttp from 'pino-http';
+
 export const appConfig = (): http.Server => {
   const app = express();
+  const logger = pino();
+
+  app.use(pinoHttp({ logger }));
+
+  app.get('/', (req, res) => {
+    req.log.info('Rota raiz acessada');
+    res.send('Hello World!');
+  });
 
   app.use(cors());
   app.use(express.json({ limit: '1mb' }));
